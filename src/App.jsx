1f857ff30ca57ac54948dbc21ae5ae412a7b013f
Toufik <toufikamin2000@gmail.com>
2026-09-19 17:45:26 +0600

    Resolve Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
-import MainLayout from "./layouts/mainLayout";
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 
